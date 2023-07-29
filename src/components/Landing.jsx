89b478083499e37d3358937bffa3f6cc6d1c49b5
shiva <zugobo@gmail.com>
2023-07-29 14:23:27 +0530

    incite

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import Navbar from './NavBar'
+import myImage from '../assets/inciteLogo.png'
+
 const Landing = () => {
     return (
         <div>
+            <div>
+                <img
+                    src={myImage}
+                    alt="My Image"
+                    style={{
+                        position: 'absolute',
+                        top: '10px',
+                        left: '10px',
+                        width: '200px', // Set the width and height as per your requirement
+                    }}
+                />
+            </div>
             <Navbar />
         </div>
     )
--- src/navbarComponents/Home.jsx
+++ src/navbarComponents/Home.jsx
@@ -1,23 +1,10 @@
 import React from 'react'
-import myImage from '../assets/inciteLogo.png'
 
 function Home() {
   return (
     <div>
-      <div>
-        <img
-        src={myImage}
-        alt="My Image"
-        style={{
-          position: 'absolute',
-          top: '10px',
-          left: '10px',
-          width: '200px', // Set the width and height as per your requirement
-        }}
-        />
-        </div>
-        <div className='heading-content'>
-          <h1>InciteIntellects</h1>
+        <div className="heading-content">
+          <h1>InciteIntellects</h1>    
         </div>
     </div>
   )
